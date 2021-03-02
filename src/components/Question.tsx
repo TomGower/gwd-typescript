a5eb221cb21f832b93ec7934e1f90f346ca3ccf0
TomGower <tom.gower@gmail.com>
2021-03-02 09:30:20 -0600

    Refactored to match deployed version

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -4,24 +4,22 @@ interface IProps {
   thisColor: string,
   round: number,
   answer: string,
-  qnumber: number,
-  checked: string,
+  num: number,
+  checked: string | boolean,
 }
 
 const Question : FunctionComponent<IProps> = ({
-  thisColor, round, answer, qnumber, checked = '',
+  thisColor, round, answer, num, checked = false,
 }) => (
   <div>
-    <strong>
-      Question
-      {` ${qnumber}`}
-    </strong>
+    <strong>{`Question ${num}`}</strong>
     <br />
-    <span style={{ color: thisColor }}>Answer</span>
-    {': '}
-    <input className="answer" id={`round${round}answer q${qnumber}`} defaultValue={answer} />
-    <input type="checkbox" id={`round${round} question${qnumber}`} defaultChecked={!!checked} />
-    <label htmlFor={`question${qnumber}`}>Check if correct</label>
+    <span style={{ color: thisColor }}>{'Answer: '}</span>
+    <input className="answer" id={`round${round}answer q${num}`} defaultValue={answer} />
+    <label htmlFor={`question${num}`}>
+      <input type="checkbox" id={`round${round} question${num}`} defaultChecked={!!checked} />
+      Check if correct
+    </label>
     <br />
   </div>
 );
