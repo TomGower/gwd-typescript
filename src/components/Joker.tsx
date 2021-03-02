a53f94d061efc6023eda312054d01bd35c37e11d
TomGower <tom.gower@gmail.com>
2021-03-01 19:24:18 -0600

    Refactored for 7-round format

--- src/components/Joker.tsx
+++ src/components/Joker.tsx
@@ -1,5 +1,20 @@
 import React, { FunctionComponent } from 'react';
 
+interface JokerInputProps {
+  round: number
+}
+
+const JokerInput : FunctionComponent<JokerInputProps> = ({ round }) => (
+  <>
+    <label htmlFor={`Jround${round}`}>
+      <input type="radio" id={`Jround${round}`} name="joker" value={`r${round}`} data-input={round} />
+      {`Round ${round}`}
+    </label>
+  </>
+);
+
+const rounds = [1, 2, 3, 4, 5, 6, 7];
+
 interface IProps {
   pickJoker: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
@@ -8,29 +23,7 @@ const Joker : FunctionComponent<IProps> = ({ pickJoker }) => (
   <div>
     <p>Pick which round you want to joker!</p>
     <div>
-      <input type="radio" id="Jround1" name="joker" value="r1" />
-      <label htmlFor="Jround1">Round 1</label>
-
-      <input type="radio" id="Jround2" name="joker" value="r2" />
-      <label htmlFor="Jround2">Round 2</label>
-
-      <input type="radio" id="Jround3" name="joker" value="r3" />
-      <label htmlFor="Jround3">Round 3</label>
-
-      <input type="radio" id="Jround4" name="joker" value="r4" />
-      <label htmlFor="Jround4">Round 4</label>
-
-      <input type="radio" id="Jround5" name="joker" value="r5" />
-      <label htmlFor="Jround5">Round 5</label>
-
-      <input type="radio" id="Jround6" name="joker" value="r6" />
-      <label htmlFor="Jround6">Round 6</label>
-
-      <input type="radio" id="Jround7" name="joker" value="r7" />
-      <label htmlFor="Jround7">Round 7</label>
-
-      <input type="radio" id="Jround8" name="joker" value="r8" />
-      <label htmlFor="Jround8">Round 8</label>
+      {rounds.map((round) => <JokerInput round={round} key={round} />)}
     </div>
     <div>
       <button type="submit" id="jokerButton" onClick={pickJoker}>Submit Joker Round Selection</button>
