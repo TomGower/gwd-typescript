43b911d484300a34c0c2df34efe51de44f54d4bb
TomGower <tom.gower@gmail.com>
2021-03-02 09:27:03 -0600

    Updated to match deployed version

--- src/components/TwoPartQuestion.tsx
+++ src/components/TwoPartQuestion.tsx
@@ -2,35 +2,36 @@ import React, { FunctionComponent } from 'react';
 
 interface IProps {
   round: number,
-  qnumber: number,
+  num: number,
   color1: string,
   answer1: string,
-  checked1: string,
+  checked1: string | boolean,
   color2: string,
   answer2: string,
-  checked2: string
+  checked2: string | boolean
 }
 
-const TwoPartQuestion: FunctionComponent<IProps> = ({
-  round, qnumber, color1, answer1, checked1, color2, answer2, checked2,
+const TwoPartQuestion : FunctionComponent<IProps> = ({
+  round, num, color1, answer1, checked1, color2, answer2, checked2,
 }) => (
   <div>
     <strong>
-      {'Question '}
-      {qnumber}
+      {`Question ${num}`}
     </strong>
     <br />
-    <span style={{ color: color1 }}>Answer A</span>
-    {': '}
-    <input className="answer" id={`round${round}answer q${qnumber}a`} defaultValue={answer1} />
-    <input type="checkbox" id={`round${round} question${qnumber}`} defaultChecked={!!checked1} />
-    <label htmlFor={`question${qnumber}`}>Check if correct</label>
+    <span style={{ color: color1 }}>{'Answer A: '}</span>
+    <input className="answer" id={`round${round}answer q${num}a`} defaultValue={answer1} />
+    <label htmlFor={`question${num}a`}>
+      <input type="checkbox" id={`round${round} question${num}a`} defaultChecked={!!checked1} />
+      Check if correct
+    </label>
     <br />
-    <span style={{ color: color2 }}>Answer B</span>
-    {': '}
-    <input className="answer" id={`round${round}answer q${qnumber}b`} defaultValue={answer2} />
-    <input type="checkbox" id={`round${round} question${qnumber}`} defaultChecked={!!checked2} />
-    <label htmlFor={`question${qnumber}`}>Check if correct</label>
+    <span style={{ color: color2 }}>{'Answer B: '}</span>
+    <input className="answer" id={`round${round}answer q${num}b`} defaultValue={answer2} />
+    <label htmlFor={`question${num}b`}>
+      <input type="checkbox" id={`round${round} question${num}b`} defaultChecked={!!checked2} />
+      Check if correct
+    </label>
     <br />
   </div>
 );
