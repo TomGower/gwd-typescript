1fd3e3afecc8051a0b5203b69f39e2e3cb596a1c
TomGower <tom.gower@gmail.com>
2021-03-01 19:24:10 -0600

    Refactored Qs to separate file

--- src/components/BonusQuestions.tsx
+++ src/components/BonusQuestions.tsx
@@ -1,49 +1,13 @@
 import React from 'react';
+import BonusQuestion from './BonusQuestion';
+
+const bonusRounds = [1, 3, 4, 6, 7];
 
 const BonusRound = () => (
   <div>
     <strong>Bonus Questions</strong>
     <br />
-    Round 1 Bonus Question
-    <br />
-    <span>Answer</span>
-    {': '}
-    <input className="answer" />
-    <input type="checkbox" id="bonusquestion1" />
-    <label htmlFor="bonusquestion1">Check if correct</label>
-    <br />
-    Round 3 Bonus Question
-    <br />
-    <span>Answer</span>
-    {': '}
-    <input className="answer" />
-    <input type="checkbox" id="bonusquestion3" />
-    <label htmlFor="bonusquestion3">Check if correct</label>
-    <br />
-    Round 4 Bonus Question
-    <br />
-    <span>Answer</span>
-    {': '}
-    <input className="answer" />
-    <input type="checkbox" id="bonusquestion4" />
-    <label htmlFor="bonusquestion4">Check if correct</label>
-    <br />
-    Round 6 Bonus Question
-    <br />
-    <span>Answer</span>
-    {': '}
-    <input className="answer" />
-    <input type="checkbox" id="bonusquestion6" />
-    <label htmlFor="bonusquestion6">Check if correct</label>
-    <br />
-    Round 8 Bonus Question
-    <br />
-    <span>Answer</span>
-    {': '}
-    <input className="answer" />
-    <input type="checkbox" id="bonusquestion8" />
-    <label htmlFor="bonusquestion8">Check if correct</label>
-    <br />
+    {bonusRounds.map((round) => <BonusQuestion roundNumber={round} key={round} />)}
   </div>
 );
 
